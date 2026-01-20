e9e5d67517071e8adeda8a38d9d36c897ff17d32
Sibercat <planetdima@gmail.com>
2026-01-20 13:33:55 -0500

    Add files via upload

---
+++ Views/ServerSettingsView.xaml.cs
@@ -0,0 +1,26 @@
+using System.Windows;
+using System.Windows.Controls;
+
+namespace IsleServerLauncher
+{
+    public partial class ServerSettingsView : UserControl
+    {
+        public static readonly DependencyProperty RconEnabledProperty =
+            DependencyProperty.Register(
+                nameof(RconEnabled),
+                typeof(bool),
+                typeof(ServerSettingsView),
+                new PropertyMetadata(false));
+
+        public bool RconEnabled
+        {
+            get => (bool)GetValue(RconEnabledProperty);
+            set => SetValue(RconEnabledProperty, value);
+        }
+
+        public ServerSettingsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
